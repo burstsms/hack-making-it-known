938644d30dd56b4151b5dc5378386e939a6e3263
willzhaoburstsms <will.zhao@burstsms.com>
2023-05-23 22:14:52 +1000

    pub sub example

--- function.go
+++ function.go
@@ -4,11 +4,13 @@ package mik
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/slack-go/slack"
 
 	"github.com/burstsms/hack-making-it-known/openai"
@@ -145,4 +147,46 @@ func ValidateSignature(r *http.Request) bool {
 		return false
 	}
 	return true
+}
+
+// SendToCloudTopic Publish function example
+func SendToCloudTopic(w http.ResponseWriter, r *http.Request) {
+	// Set up the Google Cloud Pub/Sub client
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, "your-project-id")
+	if err != nil {
+		log.Fatalf("Failed to create Pub/Sub client: %v", err)
+	}
+
+	// topic name
+	topicName := "projects/transmit-non-prod/topics/hack-slack-bridge"
+
+	// Get a reference to the topic
+	topic := client.Topic(topicName)
+
+	// Publish a message to the topic
+	result := topic.Publish(ctx, &pubsub.Message{
+		Data: []byte("First Test!"),
+	})
+
+	// Get the server-generated message ID
+	msgID, err := result.Get(ctx)
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+	}
+
+	fmt.Printf("Published message with ID: %s\n", msgID)
+}
+
+// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
+// additional information regarding Pub/Sub events.
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+// HelloPubSub consumes a Pub/Sub message.
+// This is a format sample
+func HelloPubSub(ctx context.Context, m PubSubMessage) error {
+	log.Println(string(m.Data))
+	return nil
 }
