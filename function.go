2b30550b11bdb3ddbcfa84d75a2f4a33bb0d0c45
willzhaoburstsms <will.zhao@burstsms.com>
2023-05-23 10:22:22 +1000

    update nmae

--- function.go
+++ function.go
@@ -12,7 +12,7 @@ import (
 
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Message string `json:"message"`
 	}
